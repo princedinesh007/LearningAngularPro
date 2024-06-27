2aea0f114d0b4c47483453bbca721ff5807714f7
Dineshkumar Arunachalam <dineshkumar@ssdi.sharp.co.in>
2024-06-27 18:17:27 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChileComponent } from './chile/chile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
+    ChileComponent
   ],
   imports: [
     BrowserModule,
